03a69ebc07785e83fd5b4a748eb69387b7b8d4cc
孙永豪 Yonghao S <yonghao.sun@davincimotor.com>
2022-09-20 14:28:39 +0800

    data

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,60 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import Carousel from './components/Carousel/index.vue'
+import imagePng from './components/Carousel/image.png'
+import { reactive } from '@vue/reactivity'
+
+const bannerList = reactive([{
+    name: 'Bar Party1', // string，用于 roll 形式的指示器
+    type: 'image', // string, 背景类型，image/video,
+    src: imagePng, // string, 背景源
+  },
+  // {
+  //   name: 'Bar Party2', // string，用于 roll 形式的指示器
+  //   type: 'image', // string, 背景类型，image/video,
+  //   src: imagePng, // string, 背景源
+  // },
+  {
+    name: 'Bar Party3', // string，用于 roll 形式的指示器
+    headline: '<div>Bar Party</div>', // string，用于大标题，v-html 绑定，可以自定义字号尺寸
+    sub: '<div>12.12 20:00 | Sweet Bistro, Beijing</div>', // string，用于副标题，v-html 绑定，可以自定义字号尺寸
+    type: 'image', // string, 背景类型，image/video,
+    src: imagePng, // string, 背景源
+    buttons: [ // 按钮，用于渲染按钮
+      {
+        text: 'Reserve Now', // 按钮文字
+        type: 'route', // 按钮功能：route - 路由跳转，link - 网页跳转，callback - 回调函数
+        to: '/activity/bar-party-1212', // 跳转目标
+        target: '_blank' // 是否打开新窗口
+      },
+      {
+        text: 'Reserve Now',
+        type: 'callback',
+        callback: () => { // 回调函数
+          console.log('callback executed')
+        }
+      }
+    ],
+    links: [ // 链接，用于渲染链接
+      {
+        text: 'Learn more',
+        type: 'route',
+        to: '/activity/bar-party-1212',
+        target: '_blank'
+      },
+      {
+        text: 'Reserve Now',
+        type: 'route',
+        to: '/reserve/bar-party-1212',
+        target: '_blank'
+      }
+    ]
+  }
+])
 </script>
 
 <template>
-  <Carousel />
+  <Carousel :data="bannerList" />
   <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
--- src/components/Carousel/index.vue
+++ src/components/Carousel/index.vue
@@ -2,7 +2,7 @@
   <div :class="`swiper mode-${mode}`">
     <div class="swiper-wrapper">
       <div
-        v-for="(item, index) in bannerList"
+        v-for="(item, index) in data"
         :data-href="item.link"
         :key="index"
         class="swiper-slide"
@@ -47,7 +47,6 @@
 <script>
 import Swiper, { Pagination, Autoplay, Navigation } from 'swiper'
 import 'swiper/swiper-bundle.css'
-import imagePng from './image.png'
 
 Swiper.use(Pagination)
 Swiper.use(Autoplay)
@@ -79,53 +78,7 @@ export default {
   data() {
     return {
       mySwiper: null,
-      bannerList: [{
-          name: 'Bar Party1', // string，用于 roll 形式的指示器
-          type: 'image', // string, 背景类型，image/video,
-          src: imagePng, // string, 背景源
-        },
-        // {
-        //   name: 'Bar Party2', // string，用于 roll 形式的指示器
-        //   type: 'image', // string, 背景类型，image/video,
-        //   src: imagePng, // string, 背景源
-        // },
-        {
-          name: 'Bar Party3', // string，用于 roll 形式的指示器
-          headline: '<div>Bar Party</div>', // string，用于大标题，v-html 绑定，可以自定义字号尺寸
-          sub: '<div>12.12 20:00 | Sweet Bistro, Beijing</div>', // string，用于副标题，v-html 绑定，可以自定义字号尺寸
-          type: 'image', // string, 背景类型，image/video,
-          src: imagePng, // string, 背景源
-          buttons: [ // 按钮，用于渲染按钮
-            {
-              text: 'Reserve Now', // 按钮文字
-              type: 'route', // 按钮功能：route - 路由跳转，link - 网页跳转，callback - 回调函数
-              to: '/activity/bar-party-1212', // 跳转目标
-              target: '_blank' // 是否打开新窗口
-            },
-            {
-              text: 'Reserve Now',
-              type: 'callback',
-              callback: () => { // 回调函数
-                console.log('callback executed')
-              }
-            }
-          ],
-          links: [ // 链接，用于渲染链接
-            {
-              text: 'Learn more',
-              type: 'route',
-              to: '/activity/bar-party-1212',
-              target: '_blank'
-            },
-            {
-              text: 'Reserve Now',
-              type: 'route',
-              to: '/reserve/bar-party-1212',
-              target: '_blank'
-            }
-          ]
-        }
-      ]
+      // bannerList: 
     }
   },
   computed: {
@@ -133,7 +86,7 @@ export default {
       return this.mode === 'card' ? 20 : 0
     },
     rollPaginationList() {
-      const list = this.bannerList.map(item => item.name);
+      const list = this.data.map(item => item.name);
       return list;
     }
   },
@@ -176,9 +129,9 @@ export default {
         spaceBetween: this.spaceBetween,
         slidesPerView: 'auto',
         centeredSlides: true,
-        // autoplay: {
-        //   delay: this.duration,
-        // },
+        autoplay: {
+          delay: this.duration,
+        },
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
